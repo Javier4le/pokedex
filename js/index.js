150627fa5e96a4ada369216f3258963cbc4f0579
Javier Rojas <javierale.rojas4@gmail.com>
2021-05-10 15:23:29 -0400

    comentario

--- js/index.js
+++ js/index.js
@@ -15,15 +15,35 @@ tinymce.init({
     content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
   });
 
+const enviarProfeOak = async function() {
+  //"this" Siempre devuelve una referencia al elemento que llamó la función, en este caso el botón
+  let nro = this.nro;
+  let res = await Swal.fire({
+    title: "¿Desea realmente continuar?",
+    text: "Está intentando enviar el pokémon al profesor Oak. Esto no se puede revertir.",
+    icon: "warning",
+    showCancelButton: true,
+    conifmButtonText: "Sí, Hazlo!"
+  });
+  if (res.isConfirmed){
+    //"splice" Para eliminar elementos de una lista y reestructurarla
+    pokemones.splice(nro, 1);
+    cargarTabla();
+    Swal.fire("Pokémon descartado", "Pokémon enviado al profesor", "info");
+  } else {
+    Swal.fire("Cancelado", "Operación cancelada", "error");
+  }
+};
+
 const pokemones = []; //Definir un array
-const cargarTabla = ()=>{
+const cargarTabla = () => {
 
   //1. Obtener una referencia a la tabla
   let tbody = document.querySelector("#tbody-tabla");
   //Eliminar el contenido de tbody
   tbody.innerHTML = "";
   //2. Recorrer la lista de pokemones
-  for(let i=0; i < pokemones.length; ++i){
+  for (let i=0; i < pokemones.length; ++i) {
     let p = pokemones[i];
     //3. Por cada pokemon generar una fila de la tabla (tr)
     let tr = document.createElement("tr");
@@ -40,21 +60,21 @@ const cargarTabla = ()=>{
 
     //TODO: El tipo tiene que ser un icono --> agregar clase dinamicamente
     let tipo = document.createElement("i");
-    if(p.tipo == "1"){
+    if (p.tipo == "1") {
       //Tipo planta <i class="fas fa-leaf"></i>
-      tipo.classList.add("fas","fa-leaf","text-success","fa-3x");
-    }else if(p.tipo == "2"){
+      tipo.classList.add("fas","fa-leaf","text-success","fa-2x");
+    } else if (p.tipo == "2") {
       //Tipo agua <i class="fas fa-tint"></i>
-      tipo.classList.add("fas","fa-tint","text-primary","fa-3x");
-    }else if(p.tipo == "3"){
+      tipo.classList.add("fas","fa-tint","text-primary","fa-2x");
+    } else if (p.tipo == "3") {
       //Tipo fuego <i class="fas fa-fire"></i>
-      tipo.classList.add("fas","fa-fire","text-danger","fa-3x");
-    }else if(p.tipo == "4"){
+      tipo.classList.add("fas","fa-fire","text-danger","fa-2x");
+    } else if (p.tipo == "4") {
       //Tipo eléctrico <i class="fas fa-bolt"></i>
-      tipo.classList.add("fas","fa-bolt","text-warning","fa-3x");
-    }else {
+      tipo.classList.add("fas","fa-bolt","text-warning","fa-2x");
+    } else {
       //Tipo normal <i class="fas fa-bullseye"></i>
-      tipo.classList.add("fas","fa-bullseye","text-info","fa-3x");
+      tipo.classList.add("fas","fa-bullseye","text-info","fa-2x");
     }
 
     tdTipo.classList.add("text-center"); // Para centrar el ícono
@@ -64,7 +84,17 @@ const cargarTabla = ()=>{
     //Cuando quiero definir derectamente el html: innerHTML
     
     tdDescripcion.innerHTML = p.descripcion;
-    //TODO: Qué hago con las acciones!
+    
+    let boton = document.createElement("button");
+    boton.classList.add("btn", "btn-danger");
+    boton.innerText = "Enviar al profesor Oak";
+
+    boton.nro = i;
+    tdAcciones.appendChild(boton);
+    tdAcciones.classList.add("text-center");
+
+    boton.addEventListener("click", enviarProfeOak);
+
     //5. Agregar los td al tr
     tr.appendChild(tdNro);
     tr.appendChild(tdNombre);
@@ -95,3 +125,9 @@ document.querySelector("#registrar-btn").addEventListener("click", ()=>{
     Swal.fire("Resultado Exitoso!", "Pokémon Registrado", "info")
     // console.log("Hola Mundo!", nombre, " ", tipo, " ", legendario);
   });
+
+
+
+  // if(total.value < 0){
+  //  Swal.fire("Ingresa un total mayor a cero") 
+  // }
